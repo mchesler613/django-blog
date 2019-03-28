c65ad76d01fbf8e492e1d820e138cdaf6258e70b
mchesler613 <merilyngm@gmail.com>
2019-03-28 00:46:44 -0400

    Added default path to blog site

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
